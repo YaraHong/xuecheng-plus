386cfcafad0fb10902ef5715d05308b486f69a1a
YaraHong <18213823950@163.com>
2024-09-17 16:29:46 +0800

    引入Nacos

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/atyaoh/SystemApplication.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/atyaoh/SystemApplication.java
@@ -3,11 +3,13 @@ package com.atyaoh;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 系统管理启动类
  */
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableSwagger2Doc
 @SpringBootApplication
